2e8f71b31369cdff3d56b775fd861532dd772c02
St33lguitarist <jlo785534@gmail.com>
2022-04-09 18:49:24 +1000

    structure organised

--- Movie_Expert.py
+++ Movie_Expert.py
@@ -3,12 +3,25 @@ def read_file():
         for data in movieData:
             newdata = data.rstrip('\n')
             movie, mainGuy = newdata.split('/')
-            print (movie)
-            print (mainGuy)
+            # print (movie)
+            # print (mainGuy)
 
 def write_file(movie, mainGuy):
     with open('VS Code/Public/movieData.txt', 'a') as data: 
         data.write('\n' + movie + '/' + mainGuy)
 
-read_file()
-write_file()
+def character_search():
+    pass
+def user_interface():
+    read_file()
+    print ("Would you like to find out more about movies?")
+    userInput = input()
+    userInput = userInput.lower()
+    if userInput == 'yes':
+        character_search()
+    elif userInput == 'no':
+        print("Ok, your loss. have a great day!")
+    else:
+        print ("I'm sorry that wasn't a yes or a no.")
+    
+user_interface()
