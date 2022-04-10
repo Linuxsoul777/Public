73d03c155fb11c3f4cd48852c953a035766bce9b
St33lguitarist <jlo785534@gmail.com>
2022-04-10 15:06:17 +1000

    terminal based prgram finished

--- Movie_Expert.py
+++ Movie_Expert.py
@@ -1,27 +1,47 @@
-def read_file():
+def write_file(movie, mainGuy):
+    with open('VS Code/Public/movieData.txt', 'a') as data: 
+        movie = movie.lower()
+        mainGuy = mainGuy.lower()
+        data.write('\n' + movie + '/' + mainGuy)
+
+
+def character_search():
+    print ("What movie would you like to find out more about? (You can type e to quit)")
+    movieInput = input()
     with open ('VS Code/Public/movieData.txt', 'r')as movieData:
         for data in movieData:
             newdata = data.rstrip('\n')
             movie, mainGuy = newdata.split('/')
-            # print (movie)
-            # print (mainGuy)
+            if movieInput.lower() == movie:
+                print (mainGuy)
+                return
+            elif movieInput.lower() == 'e':
+                print ("Ok then, Good Bye!")
+                exit()
+        if movieInput != movie:
+            print ("ERROR! Movie not filed.")
+            print ("Would you like to add this movie's details? (yes/no)")
+            answer = input()
+            if answer.lower() == 'yes':
+                print ("OH GOODY GOODY!")
+                print ("Who is the main actor in the movie?")
+                mainActor = input()
+                write_file (movieInput, mainActor)
+                return
+            elif answer.lower == 'no':
+                print ("I am sad. Have a great day!")
+                exit()
 
-def write_file(movie, mainGuy):
-    with open('VS Code/Public/movieData.txt', 'a') as data: 
-        data.write('\n' + movie + '/' + mainGuy)
+                
 
-def character_search():
-    pass
 def user_interface():
-    read_file()
     print ("Would you like to find out more about movies?")
     userInput = input()
-    userInput = userInput.lower()
-    if userInput == 'yes':
-        character_search()
-    elif userInput == 'no':
-        print("Ok, your loss. have a great day!")
-    else:
-        print ("I'm sorry that wasn't a yes or a no.")
-    
+    while userInput.lower() != 'no':
+        if userInput.lower() == 'yes':
+            character_search()
+        else:
+            print ("I'm sorry that wasn't a yes or a no.")
+            userInput = input()
+    print ("Ok, your loss. Have a great day!")
 user_interface()
