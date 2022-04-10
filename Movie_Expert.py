0c062e40bd5981f6218265a42fe77a13e0bf8916
St33lguitarist <jlo785534@gmail.com>
2022-04-10 19:18:32 +1000

    Added documentation

--- Movie_Expert.py
+++ Movie_Expert.py
@@ -1,27 +1,29 @@
 ## NOTE! Replace VS Code/Public/movieData.txt with the aboslute path of the location of this file on your computer.
 
 def write_file(movie, mainGuy, length):
+    '''creates variable to write to file movieData'''
     with open('VS Code/Public/movieData.txt', 'a') as data: 
         movie = movie.lower()
         mainGuy = mainGuy.lower()
-        data.write('\n' + movie + '/' + mainGuy + '/' + length)
+        data.write('\n' + movie + '/' + mainGuy + '/' + length)    # writes to file
 
 
 def character_search():
+    '''Creates a function to search for movie info and interact with the user'''
     print ("What movie would you like to find out more about? (You can type e to quit)")
     movieInput = input()
-    with open ('VS Code/Public/movieData.txt', 'r')as movieData:
+    with open ('VS Code/Public/movieData.txt', 'r')as movieData:            # opens movieData
         for data in movieData:
             newdata = data.rstrip('\n')
-            movie, mainGuy, length = newdata.split('/')
+            movie, mainGuy, length = newdata.split('/')                     # assigns variables for each of the nuggets of info
             if movieInput.lower() == movie:
-                print ("The main actor is " + mainGuy.title())
+                print ("The main actor is " + mainGuy.title())              # returns info for user
                 print ("The length of the movie is: " + str(length))
                 return
-            elif movieInput.lower() == 'e':
+            elif movieInput.lower() == 'e':                                 # allows for an exit command
                 print ("Ok then, Good Bye!")
                 exit()
-        if movieInput != movie:
+        if movieInput != movie:                                             # allows to file new information
             print ("ERROR! Movie not filed.")
             print ("Would you like to add this movie's details? (yes/no)")
             answer = input()
@@ -31,7 +33,7 @@ def character_search():
                 mainActor = input()
                 print ("Great, now what is the length of the movie? (Please put convert to minutes... e.g (120 minutes)")
                 movieLength = input() 
-                write_file (movieInput, mainActor, movieLength)
+                write_file (movieInput, mainActor, movieLength)             # writes to file
                 return
             elif answer.lower == 'no':
                 print ("I am sad. Have a great day!")
@@ -39,7 +41,8 @@ def character_search():
 
                 
 
-def user_interface():
+def startUp_interface():
+    '''Creates a funtion for the start-up interface'''
     print ("Would you like to find out more about movies?")
     userInput = input()
     while userInput.lower() != 'no':
@@ -49,4 +52,4 @@ def user_interface():
             print ("I'm sorry that wasn't a yes or a no.")
             userInput = input()
     print ("Ok, your loss. Have a great day!")
-user_interface()
+startUp_interface()
